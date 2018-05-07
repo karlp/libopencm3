e6e5a391071d5ff7c0d1b85a7311667fc90ccf48
Karl Palsson <karlp@tweak.net.au>
2018-05-07 23:13:06 +0000

    WIP: gadget0 f7 hacking

--- tests/gadget-zero/main-stm32f746-disco.c
+++ tests/gadget-zero/main-stm32f746-disco.c
@@ -35,18 +35,23 @@
 
 int main(void)
 {
-	rcc_clock_setup_hse(&rcc_3v3[RCC_CLOCK_3V3_216MHZ], 25e6);
+	rcc_clock_setup_hse(&rcc_3v3[RCC_CLOCK_3V3_216MHZ], 25);
 	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_SYSCFG);
 	rcc_periph_clock_enable(RCC_OTGFS);
 
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO11 | GPIO12);
-	gpio_set_af(GPIOA, GPIO_AF10, GPIO11 | GPIO12);
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10|GPIO11 | GPIO12);
+	gpio_set_af(GPIOA, GPIO_AF10, GPIO10|GPIO11 | GPIO12);
 
 	usbd_device *usbd_dev = gadget0_init(&otgfs_usb_driver, "stm32f746-disco");
+
+	rcc_periph_clock_enable(RCC_GPIOI);
+	gpio_mode_setup(GPIOI, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
 
 	ER_DPRINTF("bootup complete\n");
 	while (1) {
 		gadget0_run(usbd_dev);
+		//gpio_toggle(GPIOI, GPIO1);
 	}
 
 }
